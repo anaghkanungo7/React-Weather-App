934569d23fef3bd715202cf4f6de1a184748e88e
Anagh Kanungo <23740251+anaghkanungo7@users.noreply.github.com>
2022-01-06 19:04:09 +0530

    Initial commit

---
+++ src/components/CityCard.jsx
@@ -0,0 +1,7 @@
+const CityCard = () => {
+    return (
+        <h3>This is the city card</h3>
+    )
+}
+
+export default CityCard;
---
+++ src/components/WeatherInfoCard.jsx
@@ -0,0 +1,7 @@
+const WeatherInfoCard = () => {
+    return (
+        <h3>This is the weather info card</h3>
+    )
+}
+
+export default WeatherInfoCard;
